eea3e6aca13da403c51a75c90923d844756d3fb7
KAMENMAIL <std.67122420327@ubru.ac.th>
2026-03-21 21:53:20 +0700

    Create a Flask server with multiple routes

---
+++ server.py
@@ -0,0 +1,56 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Flask Basic</title>
+</head>
+<body>
+  <h1>Hun Sen มาแล้วจ้า</h1>
+  <hr>
+</body>
+</html>"""
+
+@app.route('/user/<name>/<age>')
+def my_name(name, age):
+  return f'<h1> My name is {name}.I\'m {age+1} years old.</h1>'
+
+@app.route('/calculator/addition/<int:a>/<int:b>')
+def addition(a,b):
+  return f'<h1>{a} + {b} = {a+b}<h1>'
+
+@app.route('/calculator/subtraction/<int:a>/<int:b>')
+def subtraction(a,b):
+  return f'<h1>{a} - {b} = {a-b}<h1>'
+
+@app.route('/calculator/multiplication/<int:a>/<int:b>')
+def multiplication(a,b):
+  return f'<h1>{a} * {b} = {a*b}<h1>'
+
+@app.route('/calculator/division/<int:a>/<int:b>')
+def division(a,b):
+  return f'<h1>{a} / {b} = {a/b}<h1>'
+
+@app.route('/calculator/mod/<int:a>/<int:b>')
+def mod(a,b):
+  return f'<h1>{a} % {b} = {a%b}<h1>'
+
+@app.route('/calculator/power/<float:base>/<float:exponent>')
+def power(base,exponent):
+  return f'<h1>{base} <sup> {base} </sup> = {base**exponent}<h1>'
+
+@app.route('/calculator/div/<int:a>/<int:b>')
+def div(a, b):
+  return f'<h1>{a} // {b} = {a//b}</h1>'
+
+
+
+#if __name__ == '__main__':
+# app.route()
